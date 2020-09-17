4420653a3609be250e51c56710fe3f941abed624
cmrivera <cmrivera0004@student.stcc.edu>
2020-09-16 20:12:08 -0400

    users-model, users-router,

---
+++ users/users-model.js
@@ -0,0 +1,29 @@
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+};
+
+const db = require("../data/config");
+
+//async function to add user. set id to await users db and then insert new user by id. return it by using find id
+async function add(user) {
+  const [id] = await db("users").insert(user);
+  return findById(id);
+}
+
+//function to find users, return the users db and select it by the id and username
+function find() {
+  return db("users").select("id", "username");
+}
+
+//function findby to find user using filter and return the users db selecting by id, username, and password.
+function findBy(filter) {
+  return db("users").select("id", "username", "password").where(filter);
+}
+
+//function to findbyid to use id and then return users db selecting it by id and username.
+function findById(id) {
+  return db("users").select("id", "username").where({ id }).first();
+}
---
+++ users/users-router.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("./users-model");
+const restrict = require("../middleware/restrict");
+
+const router = express.Router();
+
+//get users by find function from users-model and validate with restrict middleware
+router.get("/users", restrict("admin"), async (req, res, next) => {
+  try {
+    res.json(await Users.find());
+  } catch (err) {
+    next(err);
+  }
+});
+
+//post user, req body for username and password, set user and find by id with conditions
+router.post("/users", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (user) {
+      return res.status(409).json({
+        message: "Username is already taken",
+      });
+    }
+
+    const newUser = await Users.add({
+      username,
+      // hash the password with a time complexity of "14"
+      password: await bcrypt.hash(password, 14),
+    });
+
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//post login req body for username and password, and find users from users model the set conditions
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid Credentials",
+      });
+    }
+
+    // hash the password again and see if it matches what we have in the database
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordValid) {
+      return res.status(401).json({
+        message: "Invalid Credentials",
+      });
+    }
+
+    // generate a new JSON web token
+    const token = jwt.sign(
+      {
+        userID: user.id,
+        userRole: "admin", // this value would normally come from the database
+      },
+      process.env.JWT_SECRET
+    );
+
+    // send the token back as a cookie
+    res.cookie("token", token);
+
+    res.json({
+      message: `Welcome ${user.username}!`,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/logout", async (req, res, next) => {
+  try {
+    // this will delete the session in the database and try to expire the cookie,
+    // though it's ultimately up to the client if they delete the cookie or not.
+    // but it becomes useless to them once the session is deleted server-side.
+    req.session.destroy((err) => {
+      if (err) {
+        next(err);
+      } else {
+        res.status(204).end();
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
