56aea81f73f3db87bee4f87dd69ec441a8ba23fe
cmrivera <cmrivera0004@student.stcc.edu>
2020-09-18 18:21:20 -0400

    updated some items for deploy

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const server = require('./server')
+// it's recommended to load configuration for .env as early as possible
+require("dotenv").config(); // add this line as the first thing to run1
+
+const server = require("./server");
 
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`)
-})
+  console.log(`Listening on port ${PORT}...`);
+});
