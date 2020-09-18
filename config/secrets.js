cce65838ff690736cc1a6953bdbb84a3e18faf6b
cmrivera <cmrivera0004@student.stcc.edu>
2020-09-18 19:48:02 -0400

    changes made for deploy

--- config/secrets.js
+++ config/secrets.js
@@ -1,1 +1,3 @@
-JWT_SECRET = "this is our secret"
+module.exports = {
+  JWT_SECRET: process.env.JWT_SECRET || "this is our secret",
+};
--- knexfile.js
+++ knexfile.js
@@ -1,26 +1,39 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/lifehacks.db3'
+      filename: "./data/lifehacks.db3",
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds",
     },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done)
+        conn.run("PRAGMA foreign_keys = ON", done);
       },
     },
   },
-}
-
-
- 
+  //add a production
+  production: {
+    client: "sqlite3",
+    connection: {
+      database: "my_db", //name heroku guives in production, can name whatever you like
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    useNullAsDefault: true,
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
