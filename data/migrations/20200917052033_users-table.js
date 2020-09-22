12d2591af5c1bb72985e806622e1722ba485e86f
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-09-22 13:02:40 -0700

    updated the schema for database

--- data/migrations/20200917052033_users-table.js
+++ data/migrations/20200917052033_users-table.js
@@ -5,8 +5,8 @@ exports.up = function (knex, Promise) {
       tbl.string("name").unique().notNullable();
     })
     .createTable("users", (tbl) => {
-      tbl.uuid("id");
-      tbl.string("username", 128).primary().notNullable().unique();
+      tbl.increments('id').primary();
+      tbl.string("username", 128).notNullable().unique();
       // .index()
       tbl.string("password", 256).notNullable();
       tbl.string("email", 256).notNullable();
@@ -29,7 +29,7 @@ exports.up = function (knex, Promise) {
 
 exports.down = function (knex, Promise) {
   return knex.schema
-    .dropTableIfExists("posts")
+    .dropTableIfExists("howtos")
     .dropTableIfExists("users")
     .dropTableIfExists("roles");
 };
--- data/seeds/02-users.js
+++ data/seeds/02-users.js
@@ -5,21 +5,21 @@ exports.seed = async function (knex) {
   // Inserts seed entries
   await knex("users").insert([
     {
-      id: "ba746c97-7150-4f21-840d-3e9daa598292",
+      id: 1,
       username: "useone",
       password: "mypassword",
       email: "userone@email.com",
       role: "basic",
     },
     {
-      id: "afb615ba-6449-41ba-9c42-2fc24714551a",
+      id: 2,
       username: "usertwo",
       password: "mypassword",
       email: "usertwo@email.com",
       role: "admin",
     },
     {
-      id: "299f49e6-b8b9-4d49-92f1-80cc703366a1",
+      id: 3,
       username: "userthree",
       password: "mypassword",
       email: "userthree@email.com",
