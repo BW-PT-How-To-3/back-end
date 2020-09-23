a34314071c98f24ee086ceed88a55bb7d97ccabd
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-09-22 20:10:22 -0700

    updated user model

--- users/users-model.js
+++ users/users-model.js
@@ -5,12 +5,22 @@ function findById(id) {
   return db("users").select("id", "username").where({ id }).first();
 }
 
-async function addUser(user) {
-  const id = uuid.v4();
+// async function addUser() {
+//   // const id = uuid.v4();
    
-  await db("users").insert({ ...user, id });
-  return findById(id);
+//   await db("users").insert({ user });
+//   return findById(id);
+// }
+
+function addUser(user) {
+  db('users').insert(user)
+  .then(ids => {
+    return findById(ids[0]);
+  });
 }
+
+
+
 
 function allUsers() {
   return db("users");
