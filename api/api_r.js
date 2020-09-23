b49502577a37b10471f95088c1c2a35c9357cd56
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-09-23 16:07:01 -0700

    minor update

--- api/api_r.js
+++ api/api_r.js
@@ -8,5 +8,8 @@ const ur = require("../users/users_r");
 
 router.use("/howto", hr);
 router.use("/users", ur);
+router.use("/", (req, res) => {
+    res.json({ Message: "Welcome to the HowTo API!"})
+})
 
 module.exports = router;
