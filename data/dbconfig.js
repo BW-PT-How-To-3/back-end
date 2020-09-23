cd88800cb9d3d8e21a8822eff53740b5286b4c55
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-09-22 19:57:48 -0700

    updated dbconfig

--- data/dbconfig.js
+++ data/dbconfig.js
@@ -1,6 +1,8 @@
-const knex = require("knex");
+const knex = require('knex')
+const { development } = require('../knexfile')
+ 
 
-const knexfile = require("../knexfile");
+const knexfile = require('../knexfile')
+const environment = process.env.NODE_ENV || "development"
 
-//
-module.exports = knex(knexfile.development);
+module.exports = knex(knexfile[environment])
